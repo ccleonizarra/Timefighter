6f7dfb76645ae08f1d14ee3be4de976806c2d7f4
ccleonizarra <ccleonizarra@gmail.com>
2024-05-07 18:37:54 -0400

    Develop (#9)
    * adding new files (#2)
    
    adding new files
    
    * Feature/fixing double d (#3)
    
    * double d already fixed
    
    double d already fixed
    
    * Hello translation already fixed
    
    Hello translation already fixed
    
    * Line (#5)
    
    The extra line is already fixed
    
    * Word
    
    The new word is already added
    
    * Word (#6)
    
    The word is already added
    
    * View (#8)
    
    The view was modified.
    One image, text and button were added.

--- app/src/main/java/com/example/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -3,36 +3,89 @@ package com.example.timefighter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.timefighter.ui.theme.TimefighterTheme
-
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Android")
+            PreviewMessageCard()
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name! Welcome",
-            modifier = modifier
-    ) 
+@Preview
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("My name is Nico", "Jetpack Compose is great!")
+    )
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TimefighterTheme {
-        Greeting("Android")
+fun MessageCard(msg: Message) {
+    Row {
+        Column(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.nico),
+                contentDescription = "Picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Column {
+
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = msg.body,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+                
+                TheButton {
+
+                }
+            }
+        }
+    }
+}
+@Composable
+fun TheButton(onClick: () -> Unit) {
+    Row {
+        Button(onClick = { onClick() }) {
+            Text("Click")
+        }
+        Spacer(modifier = Modifier.height(2.dp))
+
     }
 }
